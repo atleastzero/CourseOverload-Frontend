bf07aef8912523c88707d170851dd6da122048ad
atleastzero <megan.obryan52@gmail.com>
2020-03-16 11:39:24 -0700

    Other forgotten updates.

--- src/containers/Backpack/Backpack.js
+++ src/containers/Backpack/Backpack.js
@@ -1,39 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react'
+import { useDrop } from 'react-dnd'
 
 import Course from '../../components/Class/Class'
 
-class Backpack extends Component {
-    state = {
-        courses: [{
-                code: "Sample123",
-                name: "Sample Name", 
-                description: "Sample Description", 
-                prereqs: null,
-                coreqs: null
-            }, {
-                code: "PSY101",
-                name: "Intro to Psych"
-            }
-        ]
-    }
+import classes from './Backpack.module.css'
 
-    render() {
-        let transformCourses = Object.keys(this.state.courses)
-        .map((key) => {
-             return [...Array(this.state.courses[key])].map((_, i) => {
-                return <Course key={key + i} courseName={this.state.courses[key].name} />;
-             });
-        })
-        .reduce((arr, el) => {
-            return arr.concat(el)
-        }, []);
+const Backpack = ({ accept, onDrop }) => {
+    var courses = [{
+            code: "Sample123",
+            name: "Sample Name", 
+            description: "Sample Description", 
+            prereqs: null,
+            coreqs: null
+        }, {
+            code: "PSY101",
+            name: "Intro to Psych"
+        }
+    ]
+    
+    const [{ isOver, canDrop }, drop] = useDrop({
+        accept,
+        drop: onDrop,
+        collect: monitor => ({
+          isOver: monitor.isOver(),
+          canDrop: monitor.canDrop(),
+        }),
+      })
 
-        return (
-            <div>
-                {transformCourses}
-            </div>
-        );
-    }
+    let transformCourses = Object.keys(this.state.courses)
+    .map((key) => {
+            return [...Array(this.state.courses[key])].map((_, i) => {
+            return <Course key={key + i} courseName={this.state.courses[key].name} />;
+            });
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+
+    return (
+        <div className={classes.Backpack}>
+            {transformCourses}
+        </div>
+    );
 }
 
 export default Backpack
